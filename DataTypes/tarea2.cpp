59fb72926dca5df79a7b6ed32af5807cfc9ac065
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-02-21 11:46:28 -0500

    Add files via upload

---
+++ DataTypes/tarea2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	bool isCodingFun = true; 
+	bool isFishTasty = false; 
+	cout << isCodingFun; //outputs 1 (true)
+	cout << isFishTasty; // autputs 0 (false)
+	return 0;
+}
