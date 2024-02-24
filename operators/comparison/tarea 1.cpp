7608d612a2aa560ef13eee5860d6e2cc38563f69
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-02-24 11:18:51 -0500

    Add files via upload

---
+++ operators/comparison/tarea 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int x = 8;
+int y = 1;
+cout << (x > y); // returns 1 (true) because 5 is greater than 3
+	return 0;
+}
