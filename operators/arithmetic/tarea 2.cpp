886d629ee6db3da0d4766cf2afbaf8df7797a3a8
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-02-23 23:35:51 -0500

    Add files via upload

---
+++ operators/arithmetic/tarea 2.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int main(int argc, char** argv) {
+	int sum1 = 100 + 50;        // 150 (100 + 50)
+    int sum2 = sum1 + 250;      // 400 (150 + 250)
+    int sum3 = sum2 + sum2;     // 800 (400 + 400)
+	return 0;
+}
