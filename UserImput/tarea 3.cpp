a59f0958fe764ad2ea9929edd65c166553d60b46
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-02-24 11:44:15 -0500

    Add files via upload

---
+++ UserImput/tarea 3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+using namespace std ; 
+int main(int argc, char** argv) {
+	int x; 
+	cout <<"type a number "; // type a number and press enter 
+	cin >> x; // get user input from the keyboard 
+	cout << "your number is :"  << x; // display the imput value 
+	return 0;
+}
---
+++ UserImput/tarea 4.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std ; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+ 
+int main(int argc, char** argv) {
+int x , y;
+int sum ; 
+cout << "type a number: ";
+cin >> x; 
+cout << "type another number: ";
+cin >> y; 
+sum = x + y;
+cout << "sum is "<< sum ; 
+
+	return 0;
+}
