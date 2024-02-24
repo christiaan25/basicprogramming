ded38c8b06a9ae866cb7a7720d9287ef91b727d7
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-02-23 21:46:55 -0500

    Add files via upload

---
+++ DataTypes/tarea 4.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	char a = 65, b = 66, c = 67; 
+	cout << a; 
+	cout << b; 
+	cout << c;
+return 0;
+}
