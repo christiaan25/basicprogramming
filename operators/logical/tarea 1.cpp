2d7fd225e929d1e32479f71897d4842b3768db26
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-02-24 11:29:46 -0500

    Add files via upload

---
+++ operators/logical/tarea 1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int x = 9;
+  int y = 4;
+  cout << (x > 4 && x < 10);
+	return 0;
+}
