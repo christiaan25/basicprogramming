b16eef1440cdcf29019fdcc2a9eb596e285b5562
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-02-23 21:57:49 -0500

    Add files via upload

---
+++ DataTypes/tarea6.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std ; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	// include the string library 
+	#include <string>
+	// create a string variable 
+	string greeting = "hello";
+	// autput string value 
+	cout << greeting; 
+	return 0;
+}
