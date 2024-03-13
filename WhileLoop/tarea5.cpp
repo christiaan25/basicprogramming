19b34484a371d7581c19c01378e6ade039dc72d8
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-03-13 11:27:21 -0500

    Add files via upload

---
+++ WhileLoop/tarea5.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int i=1;  while (1<=6){
+		cout<<"value of variable i is: "<<i<< endl; i--; 
+	}
+	return 0;
+}
