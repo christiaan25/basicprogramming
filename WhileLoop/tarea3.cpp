f2ef9a58f62f9accae8395a52c92cb39e7ede137
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-03-13 11:07:04 -0500

    Add files via upload

---
+++ WhileLoop/tarea3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+		// local variables declaration: 
+		int a = 10; 
+		// while lopp execution 
+		while (a < 20){
+			cout << "value of a: "<< a << endl; 
+			a++; 
+		}
+	
+	return 0;
+}
