d26e7a78491c09c8cc9683d3cb88a7495b15d21d
christiaan25 <160760015+christiaan25@users.noreply.github.com>
2024-03-13 11:35:52 -0500

    Add files via upload

---
+++ WhileLoop/tarea6.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std; 
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int arr[]={ 21,87,15,99, -12}; 
+	int i=0;   
+	while(i<5){
+	cout << arr [i] <<endl; 
+	i++; 
+}
+	return 0;
+
+}
